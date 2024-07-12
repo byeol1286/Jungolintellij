adfdae54b3d7afa4d54b2a232ce0d3ef2530e82f
manic-066g <yoo75583@gmail.com>
2024-07-12 10:28:14 +0900

    07.12 q150 정올

---
+++ q150.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class q150 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char[] ar = new char[10];
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = sc.next().charAt(0);
+        }
+
+        for (int i = ar.length - 1; i >= 0; i--) {
+            System.out.print(ar[i] + " ");
+        }
+    }
+}
